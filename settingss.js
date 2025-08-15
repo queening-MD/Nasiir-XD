f5411a5c933dbb4dbd629f56be4c8a8c22f4f0bd
queening-MD <abbaskaleem244244@gmail.com>
2025-08-15 23:39:34 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ZnMWySZb#UmeMb7S9s9UDRwsRNMu89Qm2l4zPJYry98ekbye4bnE",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
